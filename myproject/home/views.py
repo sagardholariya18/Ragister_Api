8b45ab852faa08721c531eb32034c3d33eb68203
Sagar Dholariya <148209811+sagardholariya18@users.noreply.github.com>
2024-02-22 15:35:10 +0530

    all set

--- myproject/home/views.py
+++ myproject/home/views.py
@@ -9,6 +9,7 @@ def home(request):
     data = request.data
     serializer = RagisterSerializer(data=data)
     if serializer.is_valid():
+        serializer.save()
+
         return Response({"status": 201, "massage": "you are wrong"})
-    serializer.save()
     return Response({"status": 200, "payload": serializer.data, "massage": "you send"})
